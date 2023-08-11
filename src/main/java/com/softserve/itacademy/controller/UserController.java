4bfb7a91337cd2f405067138e89f3196e89bc892
ElnurKasimov <ekasimov2012@gmail.com>
2023-08-12 00:22:33 +0300

    implemented create-user.html with handling errors

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -1,28 +1,54 @@
 package com.softserve.itacademy.controller;
 
+
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.service.UserService;
+import org.hibernate.JDBCException;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private  UserService userService;
 
-    //add needed fields
-
-    @GetMapping("/create")
-    public String homePage(Model model) {
-
-        return "users/create-user";
+    @Autowired
+    public UserController (UserService userService) {
+        this.userService = userService;
     }
 
-//    @PostMapping("/create")
-//    public String create(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
+
+    @GetMapping("/create")
+    public String getCreateUser(Model model) {
+        model.addAttribute("user", new User());
+        return "user/create-user";}
+
+    @PostMapping("/create")
+    public String postCreateUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("errors", bindingResult.getAllErrors());
+            return "user/create-user";
+        }
+        try{
+            userService.create(user);
+        } catch (ConstraintViolationException constraintViolationException) {
+            constraintViolationException.printStackTrace();
+        } catch (JDBCException jdbcException) {
+            jdbcException.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return "todo-lists";
+    }
+
 //
 //    @GetMapping("
 //    ")
